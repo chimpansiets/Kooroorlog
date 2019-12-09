788eb71ed76fb75eb14313d78cc8ff7482b4451d
Annelein Van reijen <avan-rei@f1r1s4.codam.nl>
2019-12-09 18:08:53 +0100

    check

--- vm/srcs/move_cursor.c
+++ vm/srcs/move_cursor.c
@@ -6,7 +6,7 @@
 /*   By: avan-rei <marvin@codam.nl>                   +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2019/12/05 16:13:26 by avan-rei       #+#    #+#                */
-/*   Updated: 2019/12/09 16:02:29 by svoort        ########   odam.nl         */
+/*   Updated: 2019/12/09 18:08:22 by avan-rei      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -59,9 +59,11 @@ void	move_cursor_to_next_operation(t_cursor *cursor, uint8_t arena[MEM_SIZE])
 		i++;
 	}
 	cursor->position = new_position;
+	ft_printf("b: cursor %i: %i\n", cursor->id, cursor->position);
 }
 
 void	move_cursor_to_next_byte(t_cursor *cursor, uint8_t arena[MEM_SIZE])
 {
 	cursor->position = (cursor->position + 1) % MEM_SIZE;
+	ft_printf("a: cursor %i: %i\n", cursor->id, cursor->position);
 }
